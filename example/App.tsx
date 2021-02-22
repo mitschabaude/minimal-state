f22b394f938880e853d86d80174943bb87b20e07
Gregor <gregor.mitscha-baude@gmx.at>
2021-02-22 15:07:25 +0100

    fix minification, use esbuild

--- example/App.tsx
+++ example/App.tsx
@@ -1,7 +1,6 @@
 import {render} from 'react-dom';
 import React from 'react';
-import State from 'minimal-state/state';
-import {use} from 'minimal-state/use';
+import State, {use} from 'minimal-state';
 
 type Todo = {name: string; done: boolean};
 const state = State({
